8f55520270d9886effd7a8f2f5eaac473fd47c5f
Sainandha <nandha8987@gmail.com>
2023-08-29 14:12:48 +0530

    NAN20230829
    Final Documents

--- public/Signup/forgot/script.js
+++ public/Signup/forgot/script.js
@@ -1,23 +1,31 @@
-import { getAuth, sendPasswordResetEmail } from "firebase/auth";
-let reset = document.querySelector("#forgot")
-reset.addEventListener("click",function(){
-let email = document.querySelector("#email3") 
+const firebaseConfig = {
+  apiKey: "AIzaSyD29IoBEIkX3U5eM3wWhUROcKXkmdMuD9k",
+  authDomain: "learncave-1dd2b.firebaseapp.com",
+  projectId: "learncave-1dd2b",
+  storageBucket: "learncave-1dd2b.appspot.com",
+  messagingSenderId: "117954623929",
+  appId: "1:117954623929:web:85becf7d3f19db19269e68",
+  measurementId: "G-3VNLV8J6SV"
+};
 
-    const auth = getAuth();
-    sendPasswordResetEmail(auth, email.value)
+firebase.initializeApp(firebaseConfig); 
+const auth = firebase.auth(); 
+
+
+function reset(){
+let email = document.getElementById("email3").value
+console.log(email)
+  
+    auth.sendPasswordResetEmail(email)
       .then(() => {
         console.log("Reset password link sent");
+        alert("Reset instructions sent to your mail!")
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
+        alert(errorMessage)
         // ..
       });
 
-
-
-
-
-
-
-})
+}
--- public/Signup/index.js
+++ public/Signup/index.js
@@ -1,8 +1,8 @@
 let container = document.getElementById('container')
 
 toggle = () => {
-	container.classList.toggle('sign-in')
 	container.classList.toggle('sign-up')
+	container.classList.toggle('sign-in')
 }
 
 setTimeout(() => {
